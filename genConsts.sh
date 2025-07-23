7c580ffc39d3bd6ad41b9bf4ba34ec889335fb82
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-23 07:10:47 +0100

    re-formatted

--- genConsts.sh
+++ genConsts.sh
@@ -10,7 +10,7 @@ done < <(
 	{
 		echo -e "CONC\nCONT";
 		grep "::" types.gen | cut -d':' -f3- | grep -v "^(" | tr ':' '\n';
-		grep "	[A-Z]" structures.gen | cut -d':' -f1 | tr -d '	'
+		grep "	[A-Z]" structures.gen | cut -d':' -f1 | tr -d '	';
 	} | sort | uniq;
 );
 
@@ -23,13 +23,12 @@ package gedcom
 const (
 HEREDOC
 
-
 	while read line; do
 		name="$(echo "$line" | tr -d "-" | tr -d "/")";
 		echo -n "	c$name";
 		pad=$(( $maxLength - ${#name} ));
 		while [ $pad -gt 0 ]; do
-			let "pad--"
+			let "pad--";
 			echo -n " ";
 		done;
 		echo " = \"$line\"";
@@ -37,7 +36,7 @@ HEREDOC
 		{
 			echo -e "0\n1\n2\n3\nCONC\nCONT";
 			grep "::" types.gen | cut -d':' -f3- | grep -v "^(" | tr ':' '\n';
-			grep "	[A-Z]" structures.gen | cut -d':' -f1 | tr -d '	'
+			grep "	[A-Z]" structures.gen | cut -d':' -f1 | tr -d '	';
 		} | sort | uniq;
 	);
 
