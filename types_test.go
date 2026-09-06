7b0f4f2cf6f06e87d3a12136143de1ef5a2014e9
Michael Woolnough <michael.woolnough@gmail.com>
2026-09-06 18:31:09 +0100

    added AddressLine tests

--- types_test.go
+++ types_test.go
@@ -39,11 +39,22 @@ func testType[T any, U pointerOf[T]](t *testing.T, tests []typeTests[T]) {
 	}
 }
 
-func l(v string) Line {
+func l(v string, subs ...Line) Line {
 	return Line{
 		line: line{
 			value: v,
 		},
+		Sub: subs,
+	}
+}
+
+func lt(v, t string, subs ...Line) Line {
+	return Line{
+		line: line{
+			tag:   t,
+			value: v,
+		},
+		Sub: subs,
 	}
 }
 
@@ -81,6 +92,57 @@ func TestAddressCountry(t *testing.T) {
 			Line:   l(strings.Repeat("a", 61)),
 			Error:  ErrInvalidLength{"AddressCountry", strings.Repeat("a", 61), 1, 60},
 			Result: AddressCountry(""),
+		},
+	})
+}
+
+func TestAddressLine(t *testing.T) {
+	testType(t, []typeTests[AddressLine]{
+		{
+			Line:   l(""),
+			Error:  ErrInvalidLength{"AddressLine", "", 1, 60},
+			Result: AddressLine(""),
+		},
+		{
+			Line:   l(strings.Repeat("a", 61)),
+			Error:  ErrInvalidLength{"AddressLine", strings.Repeat("a", 61), 1, 60},
+			Result: AddressLine(""),
+		},
+		{
+			Line:   l("a"),
+			Result: AddressLine("a"),
+		},
+		{
+			Line:   l("a", lt("", cCONT)),
+			Error:  ErrContext{"AddressLine", cCONT, ErrInvalidLength{"AddressLine", "", 1, 60}},
+			Result: AddressLine("a\n"),
+		},
+		{
+			Line:   l("a", lt(strings.Repeat("b", 61), cCONT)),
+			Error:  ErrContext{"AddressLine", cCONT, ErrInvalidLength{"AddressLine", strings.Repeat("b", 61), 1, 60}},
+			Result: AddressLine("a\n"),
+		},
+		{
+			Line:   l("a", lt("b", cCONT)),
+			Result: AddressLine("a\nb"),
+		},
+		{
+			Line:   l("a", lt("b", cCONT), lt("c", cCONT)),
+			Result: AddressLine("a\nb\nc"),
+		},
+		{
+			Line:   l("a", lt("", cCONC)),
+			Error:  ErrContext{"AddressLine", cCONC, ErrInvalidLength{"AddressLine", "", 1, 60}},
+			Result: AddressLine("a"),
+		},
+		{
+			Line:   l("a", lt(strings.Repeat("b", 61), cCONC)),
+			Error:  ErrContext{"AddressLine", cCONC, ErrInvalidLength{"AddressLine", strings.Repeat("b", 61), 1, 60}},
+			Result: AddressLine("a"),
+		},
+		{
+			Line:   l("a", lt("b", cCONC)),
+			Result: AddressLine("ab"),
 		},
 	})
 }
